0b48cb1e4078c6aea234fee89d48e677534ecc1f
Asaf Gerchak <asaf.gerchak@gmail.com>
2020-12-03 14:00:18 -0400

    initial commit :rocket::rocket::rocket:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Catalogue from './Catalogue';
+import MovieDetails from './MovieDetails';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <header>
+            <h1>Hackflix</h1>
+          </header>
+
+          <Route exact path="/" component={Catalogue} />
+          <Route exact path="/movie/:movieID" component={MovieDetails} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Catalogue.js
@@ -0,0 +1,56 @@
+import { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+
+class Catalogue extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: []
+    }
+  }
+
+  componentDidMount() {
+    axios({
+      url: `https://api.themoviedb.org/3/discover/movie`,
+      method: `GET`,
+      responseType: `json`,
+      params: {
+        api_key: 'f012df5d63927931e82fe659a8aaa3ac',
+        language: 'en-US',
+        sort_by: 'popularity.desc',
+        include_adult: 'false',
+        include_video: 'false',
+        page: 1,
+        primary_release_year: 1999
+      }
+    })
+      .then((res) => {
+        // console.log(res.data.results);
+
+        this.setState({
+          movies: res.data.results
+        })
+      })
+  }
+
+  render() {
+    return(
+      <div className="catalogue">
+        {
+          this.state.movies.map((singleMovieObject) => {
+            return (
+              <div key={singleMovieObject.id} className="movie">
+                <Link to={`/movie/${singleMovieObject.id}`}>
+                  <img src={`http://image.tmdb.org/t/p/w500/${singleMovieObject.poster_path}`} alt={`Poster for ${singleMovieObject.original_title}`} />
+                </Link>
+              </div>
+            )
+          })
+        }
+      </div>
+    )
+  }
+}
+
+export default Catalogue
